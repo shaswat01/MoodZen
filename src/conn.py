8161ad29b57f60f92c8e61d13653b59e0001dbb0
Shaswat Rajput <shaswatrajput@outlook.com>
2020-12-02 21:02:14 -0600

    updated dash details

--- src/conn.py
+++ src/conn.py
@@ -250,5 +250,32 @@ if st.sidebar.button("Get Recommendations") or session_state.checkboxed:
 
 else:
     st.subheader("About This App")
-    st.subheader("Dataset Link")
-    st.subheader("Contributers")
+    st.markdown("Welcome to MoodZen! This project is a music recommendation engine that takes into account the emotions that a particular song evokes. Emotions associated with a song are an abstract concept and can be subjective, that's why our system allows you to update the representation of emotion associated with each song that our model predicted. Over time, as more people use this web app, the model performance will improve and give emotion predictions that are closer to ground truth (or at least what majority of people believe the emotional ratings of songs to be).") 
+
+    st.markdown("For our emotion prediction, we use a Support Vector Regressor (SVR). We predict four emotions:")
+    st.markdown('- Anger')
+    st.markdown('- Sadness')
+    st.markdown('- Happiness')
+    st.markdown('- Tenderness')
+
+    st.markdown('Our predictions are a numerical representation of the particular emotion ranging from 1-5. (For example, a song with an anger rating of 4 would probably evoke anger in the listener.) The predictions are purely based on the audio features of the song, **NOT on the lyrics**, extracted using the python library Librosa.')
+
+    st.markdown('The search engine we used is based on Spotify API. Currently we can only give recommendations based on songs that we can download audio snippets of, using the Spotify API, but if a song cannot be downloaded or found, our recommendation system does not support that (12.2.2020).')
+
+    st.markdown('Please feel free to play with our recommendation system, we are very proud of it :)')
+
+    st.markdown('(It will also get better the more you use it ;))")')
+    st.subheader("Dataset")
+    st.markdown('-- As of 12.2.2020 --')
+
+    st.markdown('The recommendations we will provide are based on the dataset of songs and their emotional features that we have collected. This dataset has been curated by using Spotify’s Top 100 Listened to songs and the ratings associated with them have been measured by our team members.')
+
+    st.markdown('The dataset will update as you search for songs that are not in the dataset and also will update when you are not satisfied with the predictions associated with a particular song.  ')
+
+    st.markdown('We have two main datasets (You can find these in the repository of the project). ')
+    st.markdown('One dataset (song_df.csv) consists of songs and their emotional ratings.')
+    st.markdown('Another dataset, consists of songs and the features that we extracted from them. ')
+    st.subheader("Links")
+    st.markdown('GitHub Repository : https://github.com/shaswat01/MoodZen')
+
+    st.markdown('Data Link :  https://github.com/shaswat01/MoodZen/tree/main/src/data ')
