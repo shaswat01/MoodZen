d6583dc084ef5cf6700db4f813af45e942e5903d
Shaswat Rajput <shaswatrajput@outlook.com>
2020-12-02 22:24:23 -0600

    removed dir error

--- src/feature_extract_helper.py
+++ src/feature_extract_helper.py
@@ -72,98 +72,98 @@ def extract_feature(path):
     
     
     # Traversing over each file in path
-    file_data = [f for f in listdir(path) if isfile (join(path, f))]
-    for line in file_data:
-        if ( line[-1:] == '\n' ):
-            line = line[:-1]
+    # file_data = [f for f in listdir(path) if isfile (join(path, f))]
+    # for line in file_data:
+    #     if ( line[-1:] == '\n' ):
+    #         line = line[:-1]
 
         # Reading Song
-        songname = path + line
-        y, sr = librosa.load(songname, duration=60)
-        S = np.abs(librosa.stft(y))
-        
-        # Extracting Features
-        tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
-        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-        chroma_cq = librosa.feature.chroma_cqt(y=y, sr=sr)
-        chroma_cens = librosa.feature.chroma_cens(y=y, sr=sr)
-        melspectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
-        rmse = librosa.feature.rms(y=y)
-        cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-        contrast = librosa.feature.spectral_contrast(S=S, sr=sr)
-        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-        poly_features = librosa.feature.poly_features(S=S, sr=sr)
-        tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
-        zcr = librosa.feature.zero_crossing_rate(y)
-        harmonic = librosa.effects.harmonic(y)
-        percussive = librosa.effects.percussive(y)
-        
-        mfcc = librosa.feature.mfcc(y=y, sr=sr)
-        mfcc_delta = librosa.feature.delta(mfcc)
+    songname = path
+    y, sr = librosa.load(songname, duration=60)
+    S = np.abs(librosa.stft(y))
     
-        onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
-        frames_to_time = librosa.frames_to_time(onset_frames[:20], sr=sr)
-        
-        # Transforming Features
-        songname_vector.at[id] =  line  # song name
-        tempo_vector.at[id]= tempo  # tempo
-        total_beats.at[id] =  sum(beats)  # beats
-        average_beats.at[id] =  np.average(beats)
-        chroma_stft_mean.at[id]= np.mean(chroma_stft)  # chroma stft
-        chroma_stft_std.at[id] = np.std(chroma_stft)
-        chroma_stft_var.at[id]= np.var(chroma_stft)
-        chroma_cq_mean.at[id]= np.mean(chroma_cq)  # chroma cq
-        chroma_cq_std.at[id]= np.std(chroma_cq)
-        chroma_cq_var.at[id]= np.var(chroma_cq)
-        chroma_cens_mean.at[id]= np.mean(chroma_cens) # chroma cens
-        chroma_cens_std.at[id] = np.std(chroma_cens)
-        chroma_cens_var.at[id]= np.var(chroma_cens)
-        mel_mean.at[id]= np.mean(melspectrogram)  # melspectrogram
-        mel_std.at[id]= np.std(melspectrogram)
-        mel_var.at[id]= np.var(melspectrogram)
-        mfcc_mean.at[id]= np.mean(mfcc)  # mfcc
-        mfcc_std.at[id]= np.std(mfcc)
-        mfcc_var.at[id]= np.var(mfcc)
-        mfcc_delta_mean.at[id]= np.mean(mfcc_delta)  # mfcc delta
-        mfcc_delta_std.at[id]= np.std(mfcc_delta)
-        mfcc_delta_var.at[id]= np.var(mfcc_delta)
-        rmse_mean.at[id]= np.mean(rmse)  # rmse
-        rmse_std.at[id]= np.std(rmse)
-        rmse_var.at[id]= np.var(rmse)
-        cent_mean.at[id]= np.mean(cent)  # cent
-        cent_std.at[id]= np.std(cent)
-        cent_var.at[id]= np.var(cent)
-        spec_bw_mean.at[id]= np.mean(spec_bw)  # spectral bandwidth
-        spec_bw_std.at[id]= np.std(spec_bw)
-        spec_bw_var.at[id]= np.var(spec_bw)
-        contrast_mean.at[id]= np.mean(contrast)  # contrast
-        contrast_std.at[id]= np.std(contrast)
-        contrast_var.at[id]= np.var(contrast)
-        rolloff_mean.at[id]= np.mean(rolloff)  # rolloff
-        rolloff_std.at[id]= np.std(rolloff)
-        rolloff_var.at[id]= np.var(rolloff)
-        poly_mean.at[id]= np.mean(poly_features)  # poly features
-        poly_std.at[id]= np.std(poly_features)
-        poly_var.at[id]= np.var(poly_features)
-        tonnetz_mean.at[id]= np.mean(tonnetz)  # tonnetz
-        tonnetz_std.at[id]= np.std(tonnetz)
-        tonnetz_var.at[id]= np.var(tonnetz)
-        zcr_mean.at[id]= np.mean(zcr)  # zero crossing rate
-        zcr_std.at[id]= np.std(zcr)
-        zcr_var.at[id]= np.var(zcr)
-        harm_mean.at[id]= np.mean(harmonic)  # harmonic
-        harm_std.at[id]= np.std(harmonic)
-        harm_var.at[id]= np.var(harmonic)
-        perc_mean.at[id]= np.mean(percussive)  # percussive
-        perc_std.at[id]= np.std(percussive)
-        perc_var.at[id]= np.var(percussive)
-        frame_mean.at[id]= np.mean(frames_to_time)  # frames
-        frame_std.at[id]= np.std(frames_to_time)
-        frame_var.at[id]= np.var(frames_to_time)
-        
-        print(songname)
-        id = id+1
+    # Extracting Features
+    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+    chroma_cq = librosa.feature.chroma_cqt(y=y, sr=sr)
+    chroma_cens = librosa.feature.chroma_cens(y=y, sr=sr)
+    melspectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+    rmse = librosa.feature.rms(y=y)
+    cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+    contrast = librosa.feature.spectral_contrast(S=S, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    poly_features = librosa.feature.poly_features(S=S, sr=sr)
+    tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
+    zcr = librosa.feature.zero_crossing_rate(y)
+    harmonic = librosa.effects.harmonic(y)
+    percussive = librosa.effects.percussive(y)
+    
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    mfcc_delta = librosa.feature.delta(mfcc)
+
+    onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+    frames_to_time = librosa.frames_to_time(onset_frames[:20], sr=sr)
+    
+    # Transforming Features
+    songname_vector.at[id] =  path  # song name
+    tempo_vector.at[id]= tempo  # tempo
+    total_beats.at[id] =  sum(beats)  # beats
+    average_beats.at[id] =  np.average(beats)
+    chroma_stft_mean.at[id]= np.mean(chroma_stft)  # chroma stft
+    chroma_stft_std.at[id] = np.std(chroma_stft)
+    chroma_stft_var.at[id]= np.var(chroma_stft)
+    chroma_cq_mean.at[id]= np.mean(chroma_cq)  # chroma cq
+    chroma_cq_std.at[id]= np.std(chroma_cq)
+    chroma_cq_var.at[id]= np.var(chroma_cq)
+    chroma_cens_mean.at[id]= np.mean(chroma_cens) # chroma cens
+    chroma_cens_std.at[id] = np.std(chroma_cens)
+    chroma_cens_var.at[id]= np.var(chroma_cens)
+    mel_mean.at[id]= np.mean(melspectrogram)  # melspectrogram
+    mel_std.at[id]= np.std(melspectrogram)
+    mel_var.at[id]= np.var(melspectrogram)
+    mfcc_mean.at[id]= np.mean(mfcc)  # mfcc
+    mfcc_std.at[id]= np.std(mfcc)
+    mfcc_var.at[id]= np.var(mfcc)
+    mfcc_delta_mean.at[id]= np.mean(mfcc_delta)  # mfcc delta
+    mfcc_delta_std.at[id]= np.std(mfcc_delta)
+    mfcc_delta_var.at[id]= np.var(mfcc_delta)
+    rmse_mean.at[id]= np.mean(rmse)  # rmse
+    rmse_std.at[id]= np.std(rmse)
+    rmse_var.at[id]= np.var(rmse)
+    cent_mean.at[id]= np.mean(cent)  # cent
+    cent_std.at[id]= np.std(cent)
+    cent_var.at[id]= np.var(cent)
+    spec_bw_mean.at[id]= np.mean(spec_bw)  # spectral bandwidth
+    spec_bw_std.at[id]= np.std(spec_bw)
+    spec_bw_var.at[id]= np.var(spec_bw)
+    contrast_mean.at[id]= np.mean(contrast)  # contrast
+    contrast_std.at[id]= np.std(contrast)
+    contrast_var.at[id]= np.var(contrast)
+    rolloff_mean.at[id]= np.mean(rolloff)  # rolloff
+    rolloff_std.at[id]= np.std(rolloff)
+    rolloff_var.at[id]= np.var(rolloff)
+    poly_mean.at[id]= np.mean(poly_features)  # poly features
+    poly_std.at[id]= np.std(poly_features)
+    poly_var.at[id]= np.var(poly_features)
+    tonnetz_mean.at[id]= np.mean(tonnetz)  # tonnetz
+    tonnetz_std.at[id]= np.std(tonnetz)
+    tonnetz_var.at[id]= np.var(tonnetz)
+    zcr_mean.at[id]= np.mean(zcr)  # zero crossing rate
+    zcr_std.at[id]= np.std(zcr)
+    zcr_var.at[id]= np.var(zcr)
+    harm_mean.at[id]= np.mean(harmonic)  # harmonic
+    harm_std.at[id]= np.std(harmonic)
+    harm_var.at[id]= np.var(harmonic)
+    perc_mean.at[id]= np.mean(percussive)  # percussive
+    perc_std.at[id]= np.std(percussive)
+    perc_var.at[id]= np.var(percussive)
+    frame_mean.at[id]= np.mean(frames_to_time)  # frames
+    frame_std.at[id]= np.std(frames_to_time)
+    frame_var.at[id]= np.var(frames_to_time)
+    
+    print(songname)
+    id = id+1
     
     # Concatenating Features into one csv and json format
     feature_set['song_name'] = songname_vector  # song name
